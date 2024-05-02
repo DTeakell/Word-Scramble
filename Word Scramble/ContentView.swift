c3b632d409c4628aee37e38dfeb1b0e0c96ebb79
Dillon Teakell <teakelld@icloud.com>
2024-05-02 12:03:05 -0400

    Initial Commit

---
+++ Word Scramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Word Scramble
+//
+//  Created by Dillon Teakell on 5/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Dillon Teakell on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
